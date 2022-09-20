8f907789c9f86310e96e330e9efca8375cd86f31
Éderson Fernandes <edersonluis.rf@gmail.com>
2022-09-20 11:19:16 -0300

    HttpClient

---
+++ frontend/src/infra/HttpClient/HttpClient.js
@@ -0,0 +1,21 @@
+// Arquitetura Hexagonal
+// Ports & Adapters
+export async function HttpClient(fetchUrl, fetchOptions) {
+  const options = {
+    ...fetchOptions,
+    headers: {
+      'Content-Type': 'application/json',
+      ...fetchOptions.headers,
+    },
+    body: fetchOptions.body ? JSON.stringify(fetchOptions.body) : null,
+  };
+  return fetch(fetchUrl, options)
+    .then(async (response) => {
+      return {
+        ok: response.ok,
+        status: response.status,
+        statusText: response.statusText,
+        body: await response.json(),
+      }
+    })
+}
